4ae459ecdfc4f4be72dccbc8b24e1e01dd137a83
harie-hunt <haripuji8@gmail.com>
2024-09-01 20:14:22 +0700

    layout

--- app-sveltekit/src/routes/(app)/+layout.svelte
+++ app-sveltekit/src/routes/(app)/+layout.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
 	import { title } from '$lib/stores';
-	import '../app.css';
 
 	// import type { LayoutData } from './$types';
 	// export let data: LayoutData;
 </script>
 
-<svelte:head>
-	<title>{$title} | Koperasi Wanita Anggrek</title>
-</svelte:head>
+<h1>{$title}</h1>
 
 <slot />
