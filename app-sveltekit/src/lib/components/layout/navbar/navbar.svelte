c0da6f6282781942f8933bf70dcb88d137787218
harie-hunt <haripuji8@gmail.com>
2024-09-01 20:37:28 +0700

    components navbar

---
+++ app-sveltekit/src/lib/components/layout/navbar/navbar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Brand from './brand.svelte';
+	import NavMenu from './nav-menu.svelte';
+</script>
+
+<div class="bg-white border-b mb-6">
+	<header class="flex items-center gap-x-6 container px-6 mx-auto">
+		<Brand />
+
+		<NavMenu />
+	</header>
+</div>
