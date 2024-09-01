ab54d53f7ba00e034fb65d37814b785254c07651
harie-hunt <haripuji8@gmail.com>
2024-09-01 20:49:51 +0700

    components profil

--- app-sveltekit/src/lib/components/layout/navbar/profil.svelte
+++ app-sveltekit/src/lib/components/layout/navbar/profil.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import { cn } from '$lib/utils/cn';
 	import Icon from '@iconify/svelte';
 </script>
 
-<button
-	class="flex hover:border-gray-400 focus:outline-none focus:border-gray-400 transition-colors items-center gap-x-1 border py-1 px-1.5 rounded-md"
->
-	<Icon icon="mdi:user-circle" class="h-5 w-5 text-gray-500" />
-	<span class="hidden md:block">nama</span>
-</button>
+<div class={cn('', $$props.class)}>
+	<button
+		class="flex hover:border-gray-400 focus:outline-none focus:border-gray-400 transition-colors items-center gap-x-1 border py-1 px-1.5 rounded-md"
+	>
+		<Icon icon="mdi:user-circle" class="h-5 w-5 text-gray-500" />
+		<span class="hidden md:block">nama</span>
+	</button>
+</div>
