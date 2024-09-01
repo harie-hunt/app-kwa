892dd65f0446c5ea4addc87b29bd5660e62969c8
harie-hunt <haripuji8@gmail.com>
2024-09-01 22:16:16 +0700

    component table sim

---
+++ app-sveltekit/src/lib/components/dashboard/dashboard-table-simpanan-pokok.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Table from '$lib/ui/table/table.svelte';
+
+	const schema = [
+		{ label: 'No', name: 'no' },
+		{ label: 'Nama', name: 'nama' }
+	];
+
+	const values: Record<string, any>[] = [
+		{ no: 'a1', nama: 'Apus' },
+		{ no: 'a2', nama: 'Sissi' }
+	];
+</script>
+
+<h2 class="mb-3 font-medium">Simpanan Pokok belum lunas</h2>
+
+<div class="rounded-md">
+	<Table {schema} {values} />
+</div>
