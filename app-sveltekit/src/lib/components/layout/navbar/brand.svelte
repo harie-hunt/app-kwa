6390e2a7125b9dbb61e07546daac26941fe994be
harie-hunt <haripuji8@gmail.com>
2024-09-01 20:37:06 +0700

    components brand

---
+++ app-sveltekit/src/lib/components/layout/navbar/brand.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { assets } from '$app/paths';
+</script>
+
+<a
+	href="/"
+	class="text-xl font-bold focus:outline-none flex items-center gap-x-2 py-3 transition-colors hover:text-blue-600 focus:text-blue-600"
+>
+	<img src="{assets}/fav_koperasi.png" alt="Logo Koperasi" class="h-8" />
+	<span>Koperasi Wanita Anggrek</span>
+</a>
