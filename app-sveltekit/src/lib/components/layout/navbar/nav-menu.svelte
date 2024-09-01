0aa43616f090b9d3649026e826d0934883ad73b3
harie-hunt <haripuji8@gmail.com>
2024-09-01 21:07:31 +0700

    fix menu

--- app-sveltekit/src/lib/components/layout/navbar/nav-menu.svelte
+++ app-sveltekit/src/lib/components/layout/navbar/nav-menu.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import MenuItem from './menu-item.svelte';
+
+	const menus: [string, string, boolean][] = [
+		['Dashboard', '/', false],
+		['Anggota', '/anggota', false],
+		['Transaksi', '/transaksi', false]
+	];
+
+	$: setmenus = menus.map((menu) => {
+		const path = $page.url.pathname;
+		const href = menu[1];
+		menu[2] = (path === '/' && href === '/') || (href !== '/' && path.includes(href));
+		return menu;
+	});
 </script>
 
-<nav class="font-medium text-gray-400">
-	<a
-		href="/"
-		class="inline-block focus:outline-none active:text-blue-600 focus:text-gray-700 hover:text-gray-700 transition-colors"
-		>adasd</a
-	>
-	<a
-		href="/"
-		class="inline-block focus:outline-none active:text-blue-600 focus:text-gray-700 hover:text-gray-700 transition-colors"
-		>adasd</a
-	>
+<nav class="font-medium text-gray-400 flex gap-x-3">
+	{#each setmenus as [label, href, active]}
+		<MenuItem {href} {label} {active} />
+	{/each}
 </nav>
