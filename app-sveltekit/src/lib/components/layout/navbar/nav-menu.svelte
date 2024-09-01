3079aa5297d6e4ca06784cbe1e009cf9c434c377
harie-hunt <haripuji8@gmail.com>
2024-09-01 20:37:16 +0700

    components menu

---
+++ app-sveltekit/src/lib/components/layout/navbar/nav-menu.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<nav class="font-medium text-gray-400">
+	<a
+		href="/"
+		class="inline-block focus:outline-none active:text-blue-600 focus:text-gray-700 hover:text-gray-700 transition-colors"
+		>adasd</a
+	>
+	<a
+		href="/"
+		class="inline-block focus:outline-none active:text-blue-600 focus:text-gray-700 hover:text-gray-700 transition-colors"
+		>adasd</a
+	>
+</nav>
