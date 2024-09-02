47bd2717d49f99d1323b718d35cf6984c7d37f72
harie-hunt <haripuji8@gmail.com>
2024-09-02 13:49:14 +0700

    fix

--- app-sveltekit/src/lib/ui/loading/loading-gadget.svelte
+++ app-sveltekit/src/lib/ui/loading/loading-gadget.svelte
@@ -2,11 +2,9 @@
 	import { fade } from 'svelte/transition';
 </script>
 
-<div transition:fade class="grid gap-6 md:grid-cols-3">
-	{#each Array.from({ length: 3 }) as _}
-		<div class="space-y-3 rounded-md bg-white p-3">
-			<div class="h-5 w-1/3 animate-pulse rounded-full bg-slate-300"></div>
-			<div class="h-5 w-2/3 animate-pulse rounded-full bg-slate-200"></div>
-		</div>
-	{/each}
-</div>
+{#each Array.from({ length: 3 }) as _}
+	<div transition:fade class="space-y-3 rounded-md bg-white p-3">
+		<div class="h-5 w-1/3 animate-pulse rounded-full bg-slate-300"></div>
+		<div class="h-6 w-2/3 animate-pulse rounded-full bg-slate-200"></div>
+	</div>
+{/each}
