e220bd9629e43d6c19e18407bf9a62f41a638b5d
harie-hunt <haripuji8@gmail.com>
2024-09-02 13:01:36 +0700

    table

--- app-sveltekit/src/lib/ui/table/tbody-cell.svelte
+++ app-sveltekit/src/lib/ui/table/tbody-cell.svelte
@@ -1,3 +1,10 @@
-<td class="p-3">
+<script lang="ts">
+	import { cn } from '$lib/utils/cn';
+
+	export let right: boolean | undefined = undefined,
+		center: boolean | undefined = undefined;
+</script>
+
+<td class={cn('py-3 px-6', { 'text-right': right, 'text-center': center })}>
 	<slot />
 </td>
--- app-sveltekit/src/lib/ui/table/thead-cell.svelte
+++ app-sveltekit/src/lib/ui/table/thead-cell.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
+	import { cn } from '$lib/utils/cn';
+
+	export let right: boolean | undefined = undefined,
+		center: boolean | undefined = undefined;
 </script>
 
-<th scope="col" class="px-3 py-1.5 font-medium text-gray-500">
+<th
+	scope="col"
+	class={cn('px-6 py-1.5 font-medium text-gray-500', {
+		'text-right': right,
+		'text-center': center
+	})}
+>
 	<slot />
 </th>
