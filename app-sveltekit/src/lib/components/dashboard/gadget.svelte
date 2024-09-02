36406ae9abc07055422887b4773f606c72eff6e9
harie-hunt <haripuji8@gmail.com>
2024-09-02 13:49:08 +0700

    add loading

--- app-sveltekit/src/lib/components/dashboard/gadget.svelte
+++ app-sveltekit/src/lib/components/dashboard/gadget.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
+	import LoadingGadget from '$lib/ui/loading/loading-gadget.svelte';
 	import { formatNumberRibuan, formatNumberRupiah } from '$lib/utils/format';
+	import type { Rekap } from '$lib/utils/type';
 	import GadgetItem from './gadget-item.svelte';
+
+	export let promise: Promise<Rekap>;
 </script>
 
-<ul class="grid md:grid-cols-3 gap-x-6">
-	<GadgetItem values={{ title: 'Anggota', nilai: 43 }} suffix="Orang" func={formatNumberRibuan} />
-	<GadgetItem values={{ title: 'Simpanan', nilai: 24560500 }} func={formatNumberRupiah} />
-	<GadgetItem values={{ title: 'Pinjaman', nilai: 10000000 }} func={formatNumberRupiah} />
+<ul class="grid gap-x-6 md:grid-cols-3">
+	{#await promise}
+		<LoadingGadget />
+	{:then value}
+		<GadgetItem
+			values={{ title: 'Anggota', nilai: value.anggota }}
+			suffix="Orang"
+			func={formatNumberRibuan}
+		/>
+		<GadgetItem values={{ title: 'Simpanan', nilai: value.simpanan }} func={formatNumberRupiah} />
+		<GadgetItem values={{ title: 'Pinjaman', nilai: value.pinjaman }} func={formatNumberRupiah} />
+	{/await}
 </ul>
