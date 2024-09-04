5a0b4ce7555aa1cfbcfc847b23431e8f0fb4b1b6
harie-hunt <haripuji8@gmail.com>
2024-09-04 12:48:15 +0700

    fix

--- app-script/dist/Type.ts
+++ app-script/dist/Type.ts
@@ -1,13 +1,18 @@
 type Parameter = { parameter: { q?: string } };
 
 type FN = "read" | "readById" | "create" | "update" | "delete";
-type SN = "anggota";
+type SN = "anggota" | 'simpanan_pokok';
 
 type ReqParams = {
     sn: SN;
     fn: FN;
     id?: string[];
     data?: Record<string, any>[];
-    _where?: [string, any];
+    _with?: string[]
     _includes?: string[];
+    _where?: [string, any];
+    _sort?: [string, "asc" | "desc"]
+    _select?: string[]
+    _unselect?: string[]
+    _order?: [number, number]
 };
